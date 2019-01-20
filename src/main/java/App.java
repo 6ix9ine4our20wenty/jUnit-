54197e9f7a7e837ee3964973a6a22f71ef147cb1
6ix9ine4our20wenty <lilpeep.6ix9ine@gmail.com>
2019-01-20 22:04:16 +0100

    Last Commit & Adding Wow Effect

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,20 +1,25 @@
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         Calculator calculator = new Calculator();
-        int valueA = 0;
-        int valueB = 0;
 
         System.out.println("Console Calculator\n" +
                 "=====================\n" +
                 "\n");
 
-        valueA = 10;
-        valueB = 20;
-        System.out.println("Summe " + valueA + " + " + valueB + " = " +  calculator.summe(valueA, valueB));
-        System.out.println("Multiplikation " + valueA + " * " + valueB +  " = " + calculator.multiplikation(valueA, valueB));
-        System.out.println("Subtraktion " + valueA + " - " + valueB +  " = " + calculator.differenz(valueA, valueB));
+        String valueA = scanner.next();
+        String valueB = scanner.next();
+
+        int value1 = Integer.parseInt(valueA);
+        int value2 = Integer.parseInt(valueB);
+
+        System.out.println("Summe " + valueA + " + " + valueB + " = " +  calculator.summe(value1, value2));
+        System.out.println("Multiplikation " + valueA + " * " + valueB +  " = " + calculator.multiplikation(value1, value2));
+        System.out.println("Subtraktion " + valueA + " - " + valueB +  " = " + calculator.differenz(value1, value2));
 
 
     }
